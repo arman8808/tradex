33cd7d5780bec3348c6a6243089d9887d3ced74a
Arman Ali <armanal3066@gmail.com>
2023-10-19 16:28:10 +0530

    commit

---
+++ script.js
@@ -0,0 +1,49 @@
+  
+ // Get all the nav links
+ const navLinks = document.querySelectorAll('.nav-link');
+const windowPathname=window.location.pathname
+ // Add click event listeners to the links
+ navLinks.forEach(link => {
+  const navLinkPathname=new URL(link.href).pathname;
+  if((windowPathname===navLinkPathname) || (windowPathname==='/index.html' && navLinkPathname==='/')){
+    link.classList.add('active')
+  }
+  //  link.addEventListener('click', function (e) {
+  //    // Remove the 'active' class from all links
+  //    navLinks.forEach(link => link.classList.remove('active'));
+
+  //    // Add the 'active' class to the clicked link
+  //    this.classList.add('active');
+     
+  //  });
+ });
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "350px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
